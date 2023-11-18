f4d0a28c59f423def8396b85bbeda23ee1f00c19
root <root@t283.mikr.us>
2023-11-18 20:28:55 +0000

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+ćimport { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  await app.listen(3232);
 }
 bootstrap();
